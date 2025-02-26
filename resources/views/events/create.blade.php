300fc878d1e11c3a4755adcecbba9c669c101e94
kyro623 <100192782+kyro623@users.noreply.github.com>
2025-02-26 19:08:12 +0100

    reszponsziv fix create blade

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -1,9 +1,33 @@
 <header>
     <title>Esemeny letrehozas</title>
     <link rel="stylesheet" href="{{ asset('assets/css/form.css') }}">
+    <!--font-awesome.min.css-->
+    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
+
+    <!--linear icon css-->
+    <link rel="stylesheet" href="{{ asset('assets/css/linearicons.css') }}">
+
+    <!--animate.css-->
+    <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}">
+
+    <!--flaticon.css-->
+    <link rel="stylesheet" href="{{ asset('assets/css/flaticon.css') }}">
+
+    <!--slick.css-->
+    <link rel="stylesheet" href="{{ asset('assets/css/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/slick-theme.css') }}">
+    
+    <!--bootstrap.min.css-->
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    
+    <!-- bootsnav -->
     <link rel="stylesheet" href="{{ asset('assets/css/bootsnav.css') }}">
+    
+    <!--style.css-->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    
+    <!--responsive.css-->
+        <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
 
 </header>
 <section class="top-area">
@@ -26,19 +50,57 @@
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse menu-ui-design" id="navbar-menu">
                     <ul class="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">
-                        <li >
+                        @auth
+                            <li>
                             <x-nav-link :href="route('events.index')">
                                     {{ __('Esemenyek') }}
                             </x-nav-link>
                         </li>
-                        <li class="active">
+                        <li  class="active">
                             <x-nav-link :href="route('events.create')">
                                     {{ __('Esemeny letrehozasa') }}
                             </x-nav-link>
                         </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <form action="">
+                                        <a href="{{ route('profile.edit') }}">
+                                            {{ __('Profile') }}
+                                        </a>
+                                    </form>
+                                    </li>
+                                    <li>
+                                        <form method="POST" action="{{ route('logout') }}">
+                                            @csrf
+                                            <a href="{{ route('logout') }}"
+                                               onclick="event.preventDefault();
+                                                         this.closest('form').submit();">
+                                                {{ __('Log Out') }}
+                                            </a>
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endauth
+                        @guest
+                            <li>
+                                <x-nav-link :href="route('login')">
+                                    {{ __('Login') }}
+                                </x-nav-link>
+                            </li>
+                            <li>
+                                <x-nav-link :href="route('register')">
+                                    {{ __('Register') }}
+                                </x-nav-link>
+                            </li>
+                        @endguest
                     </ul><!--/.nav -->
                 </div><!-- /.navbar-collapse -->
-
+                
             </div><!--/.container-->
         </nav><!--/nav-->
         <!-- End Navigation -->
@@ -47,8 +109,9 @@
 
 </section><!-- /.top-area-->
 <div class="container" id="creationform">
+    <div class="col-sm-12">
     <div class="text">
-       Contact us Form
+       Hozz letre egy esemenyt
     </div>
     <form action="#">
        <div class="form-row">
@@ -104,5 +167,6 @@
           </div>
     </form>
     </div>
+</div>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
